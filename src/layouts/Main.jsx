3ee34db02fca448536b9d071734eb69945234982
sharif-github <107545661+sharif-github@users.noreply.github.com>
2024-01-30 00:05:49 +0600

    client review section done

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -7,6 +7,11 @@ import livegirl from '../assets/images/live_girl.png';
 import coaching_icon from '../assets/icons/coaching_icon.png';
 import consultation_icon from '../assets/icons/consultation_icon.png';
 import group_coaching_icon from '../assets/icons/group_coaching_icon.png';
+import client1 from '../assets/images/client1.png';
+import client2 from '../assets/images/client2.png';
+import client3 from '../assets/images/client3.png';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const Main = () => {
     return (
@@ -65,6 +70,43 @@ const Main = () => {
                         <h5 className='mt-3 font-bold text-gray-800 text-[22px]'>Group Coaching Sessions</h5>
                         <p className='mt-2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua minim veniam.
                         </p>
+                    </div>
+                </div>
+            </section>
+
+            {/* Clients Review Section */}
+            <section className='bg-[#FAF8F6] pl-32 mb-20 py-20'>
+                <h1 className='w-[500px] leading-tight font-bold text-[48px] text-[#333461] mt-5'>Hear out what my clients say about me.</h1>
+                <div className='mt-8 grid grid-cols-3 gap-8'>
+                    <div className="bg-white p-10 border rounded-md">
+                        <p className='text-[22px]'>"I gained so much <span className='font-bold'>confidence</span> in my ability to connect and deepen my relationships with people. "</p>
+                        <h5 className='font-bold text-2xl'>Jane</h5>
+                        <img className='w-16 mt-3 mb-3' src={client1} alt="" />
+                        <Rating
+                            style={{ maxWidth: 90 }}
+                            value={5}
+                            readOnly
+                        />
+                    </div>
+                    <div className="bg-white p-10 border rounded-md">
+                        <p className='text-[22px]'>"I gained so much confidence in my ability to connect and deepen my relationships with people. "</p>
+                        <h5 className='font-bold text-2xl'>Catherine</h5>
+                        <img className='w-16 mt-3 mb-3' src={client2} alt="" />
+                        <Rating
+                            style={{ maxWidth: 90 }}
+                            value={0}
+                            readOnly
+                        />
+                    </div>
+                    <div className="bg-white p-10 border rounded-md">
+                        <p className='text-[22px]'>"I gained so much <span className='font-bold'>confidence</span> in my ability to connect and deepen my relationships with people. "</p>
+                        <h5 className='font-bold text-2xl'>Jane</h5>
+                        <img className='w-16 mt-3 mb-3' src={client3} alt="" />
+                        <Rating
+                            style={{ maxWidth: 90 }}
+                            value={3}
+                            readOnly
+                        />
                     </div>
                 </div>
             </section>
